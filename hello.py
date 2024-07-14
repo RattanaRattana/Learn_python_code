ba69939f93aed6fa08f489e18ced8723d3bfbd47
Mac <mac@Macs-MacBook-Pro.local>
2024-07-14 14:03:06 +0900

    change 3

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ import numpy as np
 
 xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
-# change 1 change 2 change3
+# change 1 change 2 change 3
 plt.plot(xpoints, ypoints)
 plt.show()
 speed = [31,111,138,28,59,77,97]
